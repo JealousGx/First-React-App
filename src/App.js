d7a17fbd68d588cef9498335f04a2887bc5dd8c9
JealousGx <jealousgx@gmail.com>
2020-06-13 14:59:14 +0500

    Updated App.js
    Added JealousGx

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p><br/>
+        </p>
         Update by JealousGx
         <a
           className="App-link"
