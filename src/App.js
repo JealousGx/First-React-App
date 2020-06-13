4facbcb46d5a33009d8f7c92136c41d353f54f7b
JealousGx <jealousgx@gmail.com>
2020-06-13 14:56:56 +0500

    Updated App.js
    Added my name in App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p><br/>
+        Update by JealousGx
         <a
           className="App-link"
           href="https://reactjs.org"
